e8fb76b0ce4212731e351aceac5a0355e02815d7
mistyk786 <mistyk786@gmail.com>
2025-02-03 00:41:51 +0800

    Added Task file and mark

--- src/main/java/Carol.java
+++ src/main/java/Carol.java
@@ -8,51 +8,78 @@ public class Carol {
     public static final String INTRO_MSG = lined(" Hello! I'm Carol, your personal assistant.\n"
             + " What can I do for you?\n");
     public static final String END_MSG = lined(" Bye! Hope to hear from you soon!\n");
-    public static ArrayList<String> list = new ArrayList<>();
+    public static final String ERROR_MSG = lined(" Please enter a valid command!\n");
+    public static ArrayList<Task> list = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         out.println(INTRO_MSG);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String msg = br.readLine();
-        boolean running = readmsg(msg);
+        String msg = br.readLine().trim();
+        boolean running = command(msg);
         while (running) {
-            msg = br.readLine();
-            running = readmsg(msg);
+            msg = br.readLine().trim();
+            running = command(msg);
         }
     }
 
-    public static boolean readmsg(String msg) {
-        if (msg.isEmpty()) {
-            out.println(lined(" Please enter a valid command!\n"));
-            return true;
-        }
-
-        switch (msg.toLowerCase()) {
-            case "bye":
-                out.println(END_MSG);
-                return false;
-            case "list":
-                if (list.isEmpty()) {
-                    out.println(lined(" Your list is empty!\n"));
-                }
-                else {
-                    StringBuilder sb = new StringBuilder();
-                    int i = 1;
-                    for (String s: list) {
-                        sb.append(" " + i++ + ". " + s + "\n");
-                    }
-                    out.println(lined(sb.toString()));
-                }
+    public static boolean command(String msg) {
+        int msglength = msg.split("\\s+").length;
+        switch (msglength) {
+            case 0:
+                out.println(ERROR_MSG);
                 return true;
+            case 1:
+                switch (msg) {
+                    case "bye":
+                        out.println(END_MSG);
+                        return false;
+                    case "list":
+                        if (list.isEmpty()) {
+                            out.println(lined(" Your list is empty!\n"));
+                        }
+                        else {
+                            StringBuilder sb = new StringBuilder();
+                            sb.append(" Here are the tasks in your list:\n");
+                            int i = 1;
+                            for (Task t: list) {
+                                sb.append(" " + i++ + "." + t.toString() + "\n");
+                            }
+                            out.println(lined(sb.toString()));
+                        }
+                        return true;
+                    default:
+                        out.println(lined(" Added: " + msg + "\n"));
+                        list.add(new Task(msg));
+                        return true;
+                }
             default:
-                out.println(lined(" Added: " + msg + "\n"));
-                list.add(msg);
-                return true;
+                switch (msg.split("\\s+")[0]) {
+                    case "mark":
+                        String[] markmsg = msg.split(" ");
+                        if (markmsg.length != 2) {
+                            out.println(ERROR_MSG);
+                        }
+                        else if (Integer.parseInt(markmsg[1]) > list.size() || Integer.parseInt(markmsg[1]) < 1) {
+                            out.println(ERROR_MSG);
+                        }
+                        else {
+                            int i = Integer.parseInt(markmsg[1]) - 1;
+                            Task t = list.get(i);
+                            t.markAsDone();
+                            String s = lined(String.format("Nice! I've marked this task as done!\n    %s\n", t));
+                            out.println(s);
+                        }
+                        return true;
+                    default:
+                        out.println(lined(" Added: " + msg + "\n"));
+                        list.add(new Task(msg));
+                        return true;
+                }
         }
     }
 
     public static String lined(String s) {
-        String line = "⸺".repeat(40) + "\n";
+        String line = "⸺".repeat(80) + "\n";
         return line + s + line;
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,30 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void markAsDone() {
+        if (!isDone) {
+            isDone = true;
+        }
+    }
+
+    public void markAsUndone() {
+        if (isDone) {
+            isDone = false;
+        }
+    }
+
+    public String toString() {
+        return String.format("[%s] %s", getStatusIcon(), description);
+    }
+}
+
