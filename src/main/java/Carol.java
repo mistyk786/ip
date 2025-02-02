cabc6f089c1782d510b1b85895b28a7976dde3b0
mistyk786 <mistyk786@gmail.com>
2025-02-02 17:43:55 +0800

    Add Carol.java and delete Duke.java

---
+++ src/main/java/Carol.java
@@ -0,0 +1,13 @@
+public class Carol {
+    public static void main(String[] args) {
+        String line = "⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺\n";
+        String intromsg = "Hello! I'm Carol, your personal assistant.\n" +
+                "What can I do for you?\n\n";
+        String endmsg = "Bye! Hope to hear from you soon!\n\n";
+        System.out.println(line
+                + intromsg
+                + line
+                + endmsg
+                + line);
+    }
+}
