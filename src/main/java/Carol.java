b110548cdfe753f7344880e8a554ec96d3baec2d
mistyk786 <mistyk786@gmail.com>
2025-02-02 23:05:14 +0800

    Add messages to list

--- src/main/java/Carol.java
+++ src/main/java/Carol.java
@@ -1,33 +1,58 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.util.InputMismatchException;
+import java.util.ArrayList;
+import static java.lang.System.out;
 
 public class Carol {
-    public static String line = "⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺\n";
-    public static String intromsg = line
-            + " Hello! I'm Carol, your personal assistant.\n"
-            + " What can I do for you?\n"
-            + line;
-    public static String endmsg = line
-        + " Bye! Hope to hear from you soon!\n"
-        + line;
+    public static final String INTRO_MSG = lined(" Hello! I'm Carol, your personal assistant.\n"
+            + " What can I do for you?\n");
+    public static final String END_MSG = lined(" Bye! Hope to hear from you soon!\n");
+    public static ArrayList<String> list = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
+        out.println(INTRO_MSG);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println(intromsg);
-        String msg = "";
-        while (true) {
+        String msg = br.readLine();
+        boolean running = readmsg(msg);
+        while (running) {
             msg = br.readLine();
-            if (msg.equals("bye")) {
-                System.out.println(endmsg);
-                return;
-            }
-            String output = String.format(" %s\n", msg);
-            System.out.println(line
-                    + output
-                    + line);
+            running = readmsg(msg);
         }
+    }
+
+    public static boolean readmsg(String msg) {
+        if (msg.isEmpty()) {
+            out.println(lined(" Please enter a valid command!\n"));
+            return true;
+        }
+
+        switch (msg.toLowerCase()) {
+            case "bye":
+                out.println(END_MSG);
+                return false;
+            case "list":
+                if (list.isEmpty()) {
+                    out.println(lined(" Your list is empty!\n"));
+                }
+                else {
+                    StringBuilder sb = new StringBuilder();
+                    int i = 1;
+                    for (String s: list) {
+                        sb.append(" " + i++ + ". " + s + "\n");
+                    }
+                    out.println(lined(sb.toString()));
+                }
+                return true;
+            default:
+                out.println(lined(" Added: " + msg + "\n"));
+                list.add(msg);
+                return true;
+        }
+    }
+
+    public static String lined(String s) {
+        String line = "⸺".repeat(40) + "\n";
+        return line + s + line;
     }
 }
