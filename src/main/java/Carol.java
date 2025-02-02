93c2191a7983e61bea6fdd19dd8299e192f14e74
mistyk786 <mistyk786@gmail.com>
2025-02-02 22:12:21 +0800

    Echo message

--- src/main/java/Carol.java
+++ src/main/java/Carol.java
@@ -1,13 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.InputMismatchException;
+
 public class Carol {
-    public static void main(String[] args) {
-        String line = "⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺\n";
-        String intromsg = "Hello! I'm Carol, your personal assistant.\n" +
-                "What can I do for you?\n\n";
-        String endmsg = "Bye! Hope to hear from you soon!\n\n";
-        System.out.println(line
-                + intromsg
-                + line
-                + endmsg
-                + line);
+    public static String line = "⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺\n";
+    public static String intromsg = line
+            + " Hello! I'm Carol, your personal assistant.\n"
+            + " What can I do for you?\n"
+            + line;
+    public static String endmsg = line
+        + " Bye! Hope to hear from you soon!\n"
+        + line;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(intromsg);
+        String msg = "";
+        while (true) {
+            msg = br.readLine();
+            if (msg.equals("bye")) {
+                System.out.println(endmsg);
+                return;
+            }
+            String output = String.format(" %s\n", msg);
+            System.out.println(line
+                    + output
+                    + line);
+        }
     }
 }
